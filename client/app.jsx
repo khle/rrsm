ee022c666c199db73f1b08def9b943b8a65c0bf5
pragmaticlogic <pragmaticlogic@gmail.com>
2016-01-04 05:55:27 +0000

    added more client and server code

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,5 @@
 var AppBar = React.createClass({
-
-    render() {
+    render() {        
         return (
             <div className="navbar-fixed">
                 <nav>
@@ -14,39 +13,81 @@ var AppBar = React.createClass({
             </div>
         );
     }
-
 });
 
-var Main = React.createClass({
-    componentDidMount: function () {
-        this.socket = io();
-    },
-    render: function() {
+var PresencePane = React.createClass({
+    render() {        
         return (
             <div>
-                <AppBar />
+                <ul className="collection">
+                    { 
+                        this.props.data.map((user, index) => {
+                            return <li className="collection-item">{user}</li>
+                        })
+                    }
+                 </ul>    
+            </div>
+        );
+    }
+});
+
+var ChatPane = React.createClass({
+    render() {
+        return (
+            <div>
                 
             </div>
         );
     }
 });
 
-React.render(<Main />, document.getElementById('container'));
- 
- 
- (function () {
-    var React = require('react'),
-        injectTapEventPlugin = require("react-tap-event-plugin");
+var Main = React.createClass({
+    getInitialState() {
+        return {
+            users: []
+        }    
+    },
+    componentDidMount() {        
+        var socket = io();
+        var props = this.props;
+        var users = this.state.users;
+        var that = this;
+        
+        socket.on('connect', function() {
+            console.log('on connect ', props);
+            socket.emit('client connect', {
+                'nickname': props.nickname
+            });
+        });
 
-    //Needed for React Developer Tools
-    window.React = React;
+        socket.on('new user', function(data) {   
+            console.log('new user');
+            console.log(data.nickname);
+            users.push(data.nickname);
+            that.setState(users);            
+        });                
+    },
+    render() {
+        return (
+            <div>
+                <AppBar />
+                <div className="row">                  
+                    <div className="col s6"><PresencePane data={this.state.users} /></div>
+                    <div className="col s6"><ChatPane /></div>
+                </div>
+            </div>
+        );
+    }
+});
 
-    //Needed for onTouchTap
-    //Can go away when react 1.0 release
-    //Check this repo:
-    //https://github.com/zilverline/react-tap-event-plugin
-    injectTapEventPlugin();
+var createRandomNickname = function(len) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for(var i = 0; i < len; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
 
-    
+React.render(<Main nickname={createRandomNickname(6)}/>, document.getElementById('container'));
 
-})();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var path = require('path'),
-    express = require('express');
+    express = require('express'),
+    Rx = require('rx');
 
-// Server part
 var app = express();
 app.use('/', express.static(path.join(__dirname, 'client')));
 
@@ -9,8 +9,24 @@ var server = app.listen(8080);
 console.log('Server listening on port 8080');
 
 var io = require('socket.io')(server);
-
-io.on('connection', function (socket) {
-    console.log('New client connected ');
+var source = Rx.Observable.create(function(observer) {
+        
+    io.on('connection', function(socket) {
+        console.log('connection');
+        socket.on('client connect', function(data) {       
+            console.log('client connect ', data);
+            observer.onNext({'socket': socket, 'data': data});       
+        });           
+    });    
     
+    return function() {
+        io.close();
+    }
 });
+
+var observer = source.subscribe(function(obj) {    
+    //obj.socket.emit('new user', obj.data);
+    io.emit('new user', obj.data);    
+    console.log('New client connected ', obj.data);
+});
+
